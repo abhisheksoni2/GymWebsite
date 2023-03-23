9445dbb6b1cbc2ab8506bbeb9ae71ff7f312abb3
Abhisek <sonideepu282@gmail.com>
2023-03-23 20:46:07 +0530

    Intial commit

---
+++ home/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+#makemigrations - create changes and store in a file
+#migrate - apply the pending changes created by makemigrations
+
+
+# Create your models here.
+class Contact(models.Model):
+    name =models.CharField(max_length=100)
+    email =models.EmailField(max_length=100)
+    phone =models.CharField(max_length=12)
+    desc = models.TextField(max_length=200)
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.name
